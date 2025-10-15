8b8959bbaad75442c452a12d10b7ae250d8e78db
Taner Özer <tanerozer16@gmail.com>
2025-10-15 15:32:26 +0300

    edited MovieDetail

--- src/components/Movie/MovieDetailPage.js
+++ src/components/Movie/MovieDetailPage.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import {
-  ScrollView,
-  ActivityIndicator,
   View,
   Text,
   StyleSheet,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRoute } from '@react-navigation/native';
 import { fetchMovieDetail } from '../../store/movie/movieDetailThunk';
 import { clearMovieDetail } from '../../store/movie/movieDetailSlice';
 
-// import MoviePoster from './sections/MoviePoster.js';
-// import OverviewSection from './sections/OverviewSection.js';
-// // import KeywordsSection from './sections/KeywordsSection';
-// import VideosSection from './sections/VideosSection.js';
-// import ExId from './sections/ExId.js';
-// // import ReviewsSection from './sections/ReviewsSection';
-// // import SimilarMoviesSection from './sections/SimilarMoviesSection';
-// // import CreditsSection from './sections/CreditsSection';
-// // import ImagesSection from './sections/ImagesSection';
-// import ProvidersSection from './sections/ProvidersSection.js';
+import MoviePoster from './sections/MoviePoster';
+import OverviewSection from './sections/OverviewSection';
+import KeywordsSection from './sections/KeywordsSection';
+import VideosSection from './sections/VideosSection';
+import ExId from './sections/ExId';
+import ReviewsSection from './sections/ReviewsSection';
+import SimilarMoviesSection from './sections/SimilarMoviesSection';
+import ImagesSection from './sections/ImagesSection';
+import ProvidersSection from './sections/ProvidersSection';
 
 export default function MovieDetailPage() {
   const route = useRoute();
@@ -46,6 +45,65 @@ export default function MovieDetailPage() {
     return () => dispatch(clearMovieDetail());
   }, [id]);
 
+  const sections = useMemo(() => {
+    const items = [];
+    if (movie)
+      items.push({
+        key: 'poster',
+        component: <MoviePoster path={movie.poster_path} />,
+      });
+    if (providers)
+      items.push({
+        key: 'providers',
+        component: <ProvidersSection providers={providers} />,
+      });
+    if (movie?.overview)
+      items.push({
+        key: 'overview',
+        component: <OverviewSection overview={movie.overview} movie={movie} />,
+      });
+    if (videos?.length)
+      items.push({
+        key: 'videos',
+        component: <VideosSection videos={videos} />,
+      });
+    if (externalIds)
+      items.push({
+        key: 'external',
+        component: <ExId externalIds={externalIds} />,
+      });
+    if (keywords?.length)
+      items.push({
+        key: 'keywords',
+        component: <KeywordsSection keywords={keywords} />,
+      });
+    if (reviews?.length)
+      items.push({
+        key: 'reviews',
+        component: <ReviewsSection reviews={reviews} />,
+      });
+    if (images?.backdrops?.length)
+      items.push({
+        key: 'images',
+        component: <ImagesSection images={images} />,
+      });
+    if (similar?.length)
+      items.push({
+        key: 'similar',
+        component: <SimilarMoviesSection movies={similar} />,
+      });
+    return items;
+  }, [
+    movie,
+    providers,
+    videos,
+    externalIds,
+    keywords,
+    reviews,
+    images,
+    similar,
+  ]);
+
   if (loading)
     return (
       <View style={styles.center}>
@@ -64,23 +122,16 @@ export default function MovieDetailPage() {
   if (!movie) return null;
 
   return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      {/* <MoviePoster path={movie.poster_path} />
-      <ProvidersSection providers={providers} />
-      <OverviewSection overview={movie.overview} movie={movie} />
-      <VideosSection videos={videos} />
-      <ExId externalIds={externalIds} />
-     <KeywordsSection keywords={keywords} />
-      <ReviewsSection reviews={reviews} />
-      <SimilarMoviesSection movies={similar} />
-      <CreditsSection credits={credits} />
-      <ImagesSection images={images} />
-      */}
-    </ScrollView>
+    <FlatList
+      data={sections}
+      keyExtractor={item => item.key}
+      renderItem={({ item }) => <View>{item.component}</View>}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 16, backgroundColor: '#0d0d0d' }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#0d0d0d', padding: 16 },
   center: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 });
