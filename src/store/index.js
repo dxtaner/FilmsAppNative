16f0d752df8f96eb39ef043c31ce8ae4776f18f3
Taner Özer <tanerozer16@gmail.com>
2025-10-16 16:08:59 +0300

    store index edited

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import popularReducer from './popular/popularSlice';
 import topRatedReducer from './topRated/topRatedSlice';
+import movieDetailReducer from './movie/movieDetailSlice';
+import personDetailReducer from './person/personDetailSlice';
 
 export const store = configureStore({
   reducer: {
     popular: popularReducer,
     topRated: topRatedReducer,
+    movieDetail: movieDetailReducer,
+    personDetail: personDetailReducer,
   },
 });
