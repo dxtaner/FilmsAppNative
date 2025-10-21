f2e9531bf593b2efd2090a33bf8dfe84930ff12f
Taner Özer <tanerozer16@gmail.com>
2025-10-21 14:59:06 +0300

    upComingAPI added

---
+++ src/store/upComing/upComingAPI.js
@@ -0,0 +1,16 @@
+// src/store/upComing/upComingAPI.js
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'd30821539f8d65f6b806f308df58ae07';
+
+export const getUpcomingMovies = async (page = 1) => {
+  const res = await axios.get(`${BASE_URL}/movie/upcoming`, {
+    params: {
+      api_key: API_KEY,
+      language: 'tr-TR',
+      page,
+    },
+  });
+  return res.data.results;
+};
